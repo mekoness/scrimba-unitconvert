9a119f69098789c9c95472b47a12e9b082569dda
mekoness <mekoness@gmail.com>
2022-10-05 10:50:41 -0700

    Working Ver 1.2

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 const button = document.getElementById("convert-btn");
-const lengthText = document.getElementById("length-txt");
-const volumeText = document.getElementById("volume-txt");
-const massText = document.getElementById("mass-txt");
-const textList = [lengthText, volumeText, massText]
+const textList = [
+    lengthText = document.getElementById("length-txt"),
+    volumeText = document.getElementById("volume-txt"),
+    massText = document.getElementById("mass-txt")
+];
 
 button.addEventListener("click", function() {
     let numToConvert = document.getElementById("num-box").value;
@@ -18,8 +13,13 @@ button.addEventListener("click", function() {
     const gallonsToLiters = numToConvert / 0.264;
     const kilosToPounds = numToConvert * 2.204;
     const poundsToKilos = numToConvert / 2.204;
-    lengthText.textContent = `${numToConvert} meters = ${metersToFeet.toFixed(2)} feet | ${numToConvert} feet = ${feetToMeters.toFixed(2)} meters`;
-    volumeText.textContent = `${numToConvert} liters = ${litersToGallons.toFixed(2)} gallons | ${numToConvert} gallons = ${gallonsToLiters.toFixed(2)} liters`;
-    massText.textContent = `${numToConvert} kilos = ${kilosToPounds.toFixed(2)} pounds | ${numToConvert} pounds = ${poundsToKilos.toFixed(2)} kilos`;
+    for (let i = 0; i < textList.length; i++) {
+        if (textList[i] === lengthText) {
+            textList[i].textContent = `${numToConvert} meters = ${metersToFeet.toFixed(3)} feet | ${numToConvert} feet = ${feetToMeters.toFixed(2)} meters`;
+        } else if (textList[i] === volumeText) {
+            textList[i].textContent = `${numToConvert} liters = ${litersToGallons.toFixed(3)} gallons | ${numToConvert} gallons = ${gallonsToLiters.toFixed(2)} liters`;
+        } else if (textList[i] === massText) {
+            textList[i].textContent = `${numToConvert} kilos = ${kilosToPounds.toFixed(3)} pounds | ${numToConvert} pounds = ${poundsToKilos.toFixed(2)} kilos`;
+        }        
+    };
 });
-    
